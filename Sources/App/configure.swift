94febb4d366cd5cd659c94ffaf5d99b2fa99d88f
Rami Alaidy <ramialaidy.ios@gmail.com>
2024-05-20 23:23:27 +0300

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,15 +9,27 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
      app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "otto.db.elephantsql.com",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "cjzngxyk",
-        password: Environment.get("DATABASE_PASSWORD") ?? "Uid2RQwuvffkLTzIEEtAN8jqh3FEb9oc",
-        database: Environment.get("DATABASE_NAME") ?? "cjzngxyk",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        
+        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
 
+        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+            password: Environment.get("DATABASE_PASSWORD") ?? "r315r199r",
+            database: Environment.get("DATABASE_NAME") ?? "postgres",
+            tls: .prefer(try .init(configuration: .clientDefault)))
+        ), as: .psql)
+    }
     app.migrations.add(CreateTodo())
     
     app.migrations.add(CreateUser())
